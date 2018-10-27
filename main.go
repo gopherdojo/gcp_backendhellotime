1e8bf4a86aa86f04c3494afb459e37463b4063b0
sinmetal <metal.tie@gmail.com>
2018-10-27 14:39:36 +0900

    add stackdriver trace

--- main.go
+++ main.go
@@ -5,16 +5,30 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"contrib.go.opencensus.io/exporter/stackdriver"
+	"go.opencensus.io/plugin/ochttp"
+	"go.opencensus.io/trace"
 )
 
 func main() {
-	http.HandleFunc("/", handler)
-
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal("ListenAndServe: ", err)
+	// Create and register a OpenCensus Stackdriver Trace exporter.
+	exporter, err := stackdriver.NewExporter(stackdriver.Options{
+		ProjectID: "metal-tile-dev1",
+	})
+	if err != nil {
+		log.Fatal(err)
 	}
+	trace.RegisterExporter(exporter)
+
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", &ochttp.Handler{}))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	ctx, span := trace.StartSpan(ctx, "/backendhellotime")
+	defer span.End()
+
 	fmt.Fprintf(w, "Hello Backend %s", time.Now())
 }
